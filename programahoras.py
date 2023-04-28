2e1325b811c42296ab97e97db0797ecdcefc22ff
MatheusBruno <matheusbrunosilveira@gmail.com>
2023-04-28 00:48:46 -0300

    .edit

--- programahoras.py
+++ programahoras.py
@@ -5,29 +5,30 @@ import os
 janela = Tk()
 janela.title("Horario")
 janela.geometry("800x450")
-janela.config(background="#110d04")
+janela.config(background="#141414")
+janela.iconbitmap()
 
 def get_horas():
     data_agora = strftime('%H:%M:%S')
     horas = strftime('%H')
     if int(horas) < 5 or int(horas) > 18 :
-        mensagem.config(text="Boa Noite")
+        mensagem.config(text=f"Boa Noite, {os.getlogin()}", fg="#b62ff5", bg="#141414")
 
     if int(horas) >= 5 and int(horas) < 12 :
-        mensagem.config(text="Bom Dia")
+        mensagem.config(text=f"Bom Dia, {os.getlogin()}", fg="#62e300", bg="#141414")
 
     if int(horas) >= 12 and int(horas) < 18 :
-        mensagem.config(text="Boa Tarde")
+        mensagem.config(text=f"Boa Tarde, {os.getlogin()}", fg="#db8b00", bg="#141414")
 
     data.config(text=data_agora)
     data.after(1000,get_horas)
     
 
-Label(font="Arial 50", text=f"Olá, {os.getlogin()}", fg="Pink", bg="#110d04").pack()
-Label(font="Arial 30", text=f"{strftime('%a, %d %b %Y')}", fg="Pink", bg="#110d04").pack()
-data = Label(font="Arial 90", fg="Pink", bg="#110d04")
-data.pack()
-mensagem = Label( font="Arial 50", fg="Pink", bg="#110d04")
+mensagem = Label( font="Arial 50")
 mensagem.pack()
+Label(font="Arial 30", text=f"{strftime('%a, %d %b %Y')}", fg="#b62ff5", bg="#141414").pack()
+data = Label(font="Arial 90", fg="#b62ff5", bg="#141414")
+data.pack()
+
 get_horas()
 janela.mainloop()
