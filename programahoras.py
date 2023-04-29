be150a7bd06389fce2bcffcbcad508a5a29286b9
MatheusBruno <matheusbrunosilveira@gmail.com>
2023-04-29 07:38:57 -0300

    .edit

--- programahoras.py
+++ programahoras.py
@@ -23,12 +23,12 @@ def get_horas():
     data.config(text=data_agora)
     data.after(1000,get_horas)
     
-Label(width="10", height="4", bg="#141414").pack()
+# # Label(width="10", height="4", bg="#141414").place(relx = 0.5, rely = 0.5,anchor=CENTER)
 mensagem = Label( font="Arial 50")
-mensagem.pack(pady=10)
-Label(font="Arial 30", text=f"{strftime('%a, %d %b %Y')}", fg="#b62ff5", bg="#141414").pack()
+mensagem.place(relx = 0.5, rely = 0.15,anchor=CENTER)
+Label(font="Arial 30", text=f"{strftime('%a, %d %b %Y')}", fg="#b62ff5", bg="#141414").place(relx = 0.5, rely = 0.3,anchor=CENTER)
 data = Label(font="Arial 90", fg="#b62ff5", bg="#141414")
-data.pack(pady=5)
+data.place(relx = 0.5, rely = 0.5,anchor=CENTER)
 
 get_horas()
 janela.mainloop()
